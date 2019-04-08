e84207a64afd8f5569cd63e9dd4d44fec7d00114
Saifuddin Merchant <saifuddin.merchant@gmail.com>
2019-04-07 20:23:50 -0400

    Update SleepUtility.java

--- BookService/src/main/java/com/example/demo/util/SleepUtility.java
+++ BookService/src/main/java/com/example/demo/util/SleepUtility.java
@@ -1,15 +1,15 @@
-package com.sam.demo.util;
+package com.example.BookService.util;
 
 import java.util.concurrent.TimeUnit;
 
 public class SleepUtility {
-	
-	public static void sleep() {
-		try {
-			TimeUnit.SECONDS.sleep(3);
-		} catch (InterruptedException e) {
 
-		}
-	}
+    public static void sleep(int timeout) {
+        try {
+            TimeUnit.SECONDS.sleep(timeout);
+        } catch (InterruptedException e) {
+
+        }
+    }
 
 }
